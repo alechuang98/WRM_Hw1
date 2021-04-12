fix normalization

src/VSM.py
@@ -22,8 +22,8 @@ class VSM(object):
                 indx = qr.index(key)
                 for v in value:
                     ctd[v[0]][indx] += v[1]
-        tf = (self.k + 1) * ctd / (ctd + self.k)
-        # tf = np.nan_to_num((tf.T / (1 - self.b + self.b * self.fileLen / self.avgLen)).T, 0)
+        tf = (self.k + 1) * ctd / (ctd.T + self.k * (1 - self.b + self.b * self.fileLen / self.avgLen)).T
+        # tf = np.nan_to_num((tf.T / self.k  ).T, 0)
         tf = np.nan_to_num(tf, 0)
         return tf
 